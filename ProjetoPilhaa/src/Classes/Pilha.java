a0f6a656730192dd4b3b2161320127ad40fdeeb5
Victor <101398802+JoaoVictor031@users.noreply.github.com>
2022-05-20 09:12:10 -0300

    Add files via upload

--- ProjetoPilhaa/src/Classes/Pilha.java
+++ ProjetoPilhaa/src/Classes/Pilha.java
@@ -10,13 +10,24 @@ public class Pilha {
 	public void empilhar(No novo) {
 		novo.setProx(this.topo);
 		this.topo = novo;
-		
 	}
+	
 	public void desempilhar(){
 		this.topo = this.topo.getProx();
-		
 	}
+	
 	public No getTopo(){
 		return this.topo;
+	}
+	
+	public void imprimirNos() {
+		if(this.topo != null) {
+			No no = this.topo;
+			while(no!= null) {
+				System.out.println(no.getInfo());
+				no = no.getProx();
+			}
+		}
+	
 	}
 }
--- ProjetoPilhaa/src/ProjetoPilha.java
+++ ProjetoPilhaa/src/ProjetoPilha.java
@@ -1,9 +1,25 @@
-
+import Classes.No;
+import Classes.Pilha;
 public class ProjetoPilha {
 
 	public static void main(String[] args) {
+		Pilha pilha = new Pilha();
+		No no = new No(10);	
+		pilha.empilhar(no);
 		
-
+		No no2 = new No(11);	
+		pilha.empilhar(no2);
+		
+		No no3 = new No(12);	
+		pilha.empilhar(no3);
+		
+		pilha.desempilhar();
+		
+		No topo = pilha.getTopo();
+		
+		//System.out.print(topo.getInfo());
+		
+		pilha.imprimirNos();
 	}
 
 }
